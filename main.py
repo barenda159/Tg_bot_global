398fc96a3e37993f0253a3bd39b320de4e64468a
barenda <yarik.rogozhnikov@bk.ru>
2025-02-10 17:00:47 +0500

    add command start

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+import telebot
+import config
 
+bot = telebot.TeleBot(config.token)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, "Приветствую вас!")
+    start(message)
