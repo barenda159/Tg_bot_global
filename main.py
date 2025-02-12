cb9334f7e9862c87b6326f09092d0f0ac735d806
Ярослав <yarik.rogozhnikov@bk.ru>
2025-02-12 20:00:45 +0500

    edit file main.py

--- main.py
+++ main.py
@@ -6,5 +6,6 @@ bot = telebot.TeleBot(config.token)
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id, "Приветствую вас!")
+    bot.send_message(message.chat.id, "Чего желаете?")
 
 bot.infinity_polling()
