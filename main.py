775af58a63273ba5df3887cb0fa14dc3f0bd3df1
Sereza <trifonovsergej089@gmail.com>
2025-02-19 19:09:16 +0400

    fix

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+from otbetka import text
 
 def text(text, bot, message):
     if text == "привет" or text == "Привет" or text == "прив" or text == "Пgрив":
@@ -7,3 +7,5 @@ def text(text, bot, message):
         bot.send_message(message.From_user.id, "бот выключен, привет реальность")
     if text == "мне грустно" or text == "Мне гнрустно":
         bot.send_message(message.From_user.id, "https://youtu.be/dQw4w9WgXcQ?si=ZgtbGoKQ2NRZWa9a")
+
+
