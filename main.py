62f969fd29cabf6ee74235995bb1c43b033652eb
danchick3526 <kofebinano@gmail.com>
2025-02-19 18:05:02 +0300

    main_kondratev

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import telebot
 import config
+from ugadaika_kondratev import ugadaika_kondratev
+
 
 bot = telebot.TeleBot(config.token)
 
@@ -7,5 +9,11 @@ bot = telebot.TeleBot(config.token)
 def start(message):
     bot.send_message(message.chat.id, "Приветствую вас!")
     bot.send_message(message.chat.id, "Чего желаете?")
+
+@bot.message_handler(commands=["ugadaika"])
+def ugadaika(message):
+    ugadaika_kondratev(message, bot)
+
+
 
 bot.infinity_polling()
