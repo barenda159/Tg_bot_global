e1eaca26564b7f43de179d3d16a2291806a2136a
keri31 <gamesdexp@gmail.com>
2025-02-19 20:12:10 +0500

    main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import telebot
 import config
-
+from help import help
 bot = telebot.TeleBot(config.token)
 
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id, "Приветствую вас!")
     bot.send_message(message.chat.id, "Чего желаете?")
-
+@bot.message_handler(commands=['help'])
+def command_help(message):
+    help(message)
 bot.infinity_polling()
