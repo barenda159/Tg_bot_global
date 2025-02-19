2a9c5a0ba0d43c2b8be7eeee757c6cb9744c2d34
Tamoke <deniskonovalov612@gmail.com>
2025-02-19 18:13:46 +0300

    main_tamoke

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import telebot
 import config
-
+from tamoke import tamoke , get_tamoke
 bot = telebot.TeleBot(config.token)
 
 @bot.message_handler(commands=['start'])
@@ -8,4 +8,6 @@ def start(message):
     bot.send_message(message.chat.id, "Приветствую вас!")
     bot.send_message(message.chat.id, "Чего желаете?")
 
+def tamoke(message,types):
+    get_tamoke()
 bot.infinity_polling()
