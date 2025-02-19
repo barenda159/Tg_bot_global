93a48bd09b7566f974af73167688efa91ae0b85b
eshkerinsky <konstantinkinzagaleev@gmail.com>
2025-02-19 20:31:01 +0500

    izm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import telebot
 import config
+from inline_kinzhagaleev import inline_kinzhagaleev
 
 bot = telebot.TeleBot(config.token)
 
@@ -7,5 +8,12 @@ bot = telebot.TeleBot(config.token)
 def start(message):
     bot.send_message(message.chat.id, "Приветствую вас!")
     bot.send_message(message.chat.id, "Чего желаете?")
+@bot.message_handler(commands=['inline'])
+
+def inline(message):
+    inline_kinzhagaleev(message, bot, types)
+
 
 bot.infinity_polling()
+
+
