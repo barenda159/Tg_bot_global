f38a31aacac73596604f0a78501ea58329c94572
Sukhanov_Nikita <nikitasuhanov06@gmail.com>
2025-02-19 18:04:01 +0300

    add wiki_Sukhanov_gen1 in main.py

--- main.py
+++ main.py
@@ -9,32 +9,9 @@ def start(message):
     bot.send_message(message.chat.id, "Приветствую вас!")
     bot.send_message(message.chat.id, "Чего желаете?")
 
-def wiki_Sukhanov(message, bot):
-    global wiki
-    if wiki:
-        bot.send_message(message.chat.id, get_wiki_Sukhanov(message.text))
-        wiki = False
-    if message.text == "Википедия":
-        bot.send_message(message.chat.id, "Напиши, что ты хочешь найти?")
-        wiki = True
-
-wikipedia.set_lang("ru")
-def get_wiki_Sukhanov(word):
-    try:
-        w = wikipedia.page(word)  # Ищем статью
-        wikitext = w.content[:400]
-        wikimas = wikitext.split(".")  # разделяем предложение
-        wikimas = wikimas[:-1]  # удаляем последний элемент из списка
-        wikiresult = ""
-        for i in wikimas:
-            if not ("==" in i):
-                wikiresult = wikiresult + i + "."
-            else:
-                break
-        wikiresult = re.sub('\([^()]*\)', "----", wikiresult)
-        wikiresult = re.sub('\{[^\{\}]*\}', "----", wikiresult)
-        return wikiresult
-    except Exception as e:
-        return "Такой статьи нет"
+@bot.message_handler(commands=['wiki'])
+def wiki(message):
+    wiki_Sukhanov(message, bot)
+    get_wiki_Sukhanov(message)
 
 bot.infinity_polling()
