cd57d9f1e2a23a71a8e196ba00e39aa7308b280e
Ярослав <yarik.rogozhnikov@bk.ru>
2025-02-19 20:12:12 +0500

    edit file main.py, new def

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import telebot
+from telebot.util import content_type_media
+
 import config
 
 bot = telebot.TeleBot(config.token)
 
 @bot.message_handler(commands=['start'])
-def start(message):
+def command_start(message):
     bot.send_message(message.chat.id, "Приветствую вас!")
     bot.send_message(message.chat.id, "Чего желаете?")
+
+@bot.message_handler(content_types=['text'])
+def feedback(message):
+    bot.reply_to(message, "К вашим услугам")
+
 
 bot.infinity_polling()
