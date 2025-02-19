c5e6aab2821bfbd263b6276a16da1a7c360cf2f8
AndreyTech <lionlonn14@gmail.com>
2025-02-19 18:33:15 +0300

    test

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import telebot
 import config
+from test_klyuew import test_klyuew
+from telebot import types
 
 bot = telebot.TeleBot(config.token)
 
@@ -7,5 +9,10 @@ bot = telebot.TeleBot(config.token)
 def start(message):
     bot.send_message(message.chat.id, "Приветствую вас!")
     bot.send_message(message.chat.id, "Чего желаете?")
+
+@bot.message_handler(commands=["test"])
+def klyew(message):
+    test_klyuew(message, bot, types)
+
 
 bot.infinity_polling()
